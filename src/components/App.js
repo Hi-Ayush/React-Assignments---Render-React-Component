9a5b41b11d40e64613fc9db0337106f07a265cce
Ayush Kumar <68886542+Hi-Ayush@users.noreply.github.com>
2020-11-09 15:19:12 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import "../styles/App.css";
 
 const App = () => {
   return (
-    <>
+    <div>
       <p>I am learning React. My life is getting better.</p>
-    </>
+      <p data-ns-test="project-name">Tinder Clone.Lol</p>
+      <p data-ns-test="project-description">Everyone knows what Tinder is!</p>
+    </div>
   );
 };
 
